4e848ea8bbd71ab0325f02f7cfdd9a3f1ca3269b
Mahdi2001 <156167981+Mahdi79-physics@users.noreply.github.com>
2026-01-30 09:22:19 +0330

    Create solution.py

---
+++ question5_error_detection/solution.py
@@ -0,0 +1,85 @@
+import pennylane as qml
+from pennylane import numpy as np
+
+
+def error_wire(circuit_output):
+    """Function that returns an error readout.
+
+    Args:
+        - circuit_output (?): the output of the `circuit` function.
+
+    Returns:
+        - (np.ndarray): a length-4 array that reveals the statistics of the
+        error channel. It should display your algorithm's statistical prediction for
+        whether an error occurred on wire `k` (k in {1,2,3}). The zeroth element represents
+        the probability that a bitflip error does not occur.
+
+        e.g., [0.28, 0.0, 0.72, 0.0] means a 28% chance no bitflip error occurs, but if one
+        does occur it occurs on qubit #2 with a 72% chance.
+    """
+
+    # QHACK #
+    P_0 = circuit_output[0] + circuit_output[7]
+    P_1 = circuit_output[3] + circuit_output[4]
+    P_2 = circuit_output[2] + circuit_output[5]
+    P_3 = circuit_output[1] + circuit_output[6]
+    list = [P_0,P_1,P_2,P_3]
+    return list
+    # process the circuit output here and return which qubit was the victim of a bitflip error!
+
+    # QHACK #
+
+
+dev = qml.device("default.mixed", wires=3)
+
+
+@qml.qnode(dev)
+def circuit(p, alpha, tampered_wire):
+    """A quantum circuit that will be able to identify bitflip errors.
+
+    DO NOT MODIFY any already-written lines in this function.
+
+    Args:
+        p (float): The bit flip probability
+        alpha (float): The parameter used to calculate `density_matrix(alpha)`
+        tampered_wire (int): The wire that may or may not be flipped (zero-index)
+
+    Returns:
+        Some expectation value, state, probs, ... you decide!
+    """
+
+    qml.QubitDensityMatrix(density_matrix(alpha), wires=[0, 1, 2])
+
+    # QHACK #
+    qml.CNOT(wires = [0,1])
+    qml.CNOT(wires = [0,2])
+    # put any input processing gates here
+
+    qml.BitFlip(p, wires=int(tampered_wire))
+
+    # put any gates here after the bitflip error has occurred
+    return qml.probs(wires = [0,1,2])
+    # return something!
+    
+    # QHACK #
+
+
+def density_matrix(alpha):
+    """Creates a density matrix from a pure state."""
+    # DO NOT MODIFY anything in this code block
+    psi = alpha * np.array([1, 0], dtype=float) + np.sqrt(1 - alpha**2) * np.array(
+        [0, 1], dtype=float
+    )
+    psi = np.kron(psi, np.array([1, 0, 0, 0], dtype=float))
+    return np.outer(psi, np.conj(psi))
+ 
+
+inputs = [0.8, 0.1234, 0]
+    # DO NOT MODIFY anything in this code block
+p, alpha, tampered_wire = inputs[0], inputs[1], int(inputs[2])
+
+error_readout = np.zeros(4, dtype=float)
+circuit_output = circuit(p, alpha, tampered_wire)
+error_readout = error_wire(circuit_output)
+
+print(*error_readout, sep=",")
